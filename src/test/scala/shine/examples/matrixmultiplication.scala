a9c55a54def7dc53ba4dfb71eac9f1087cadecc3
André <andre.hodapp@t-online.de>
2020-02-05 15:34:38 +0100

    Matrixmultiplikation wurden die auskommentierten Sachen entfernt

--- src/test/scala/shine/examples/matrixmultiplication.scala
+++ src/test/scala/shine/examples/matrixmultiplication.scala
@@ -9,7 +9,6 @@ import shine.DPIA.Semantics.OperationalSemantics.FloatData
 import shine.DPIA._
 //import shine.OpenCL.FunctionalPrimitives.{MapGlobal, OpenCLReduceSeq, To}
 //import shine.OpenCL.KernelGenerator
-//import shine.examples.dotProduct
 
 class matrixmultiplication extends test_util.Tests {
 
@@ -26,8 +25,6 @@ class matrixmultiplication extends test_util.Tests {
     val z = Identifier(freshName("z"), ExpType(float, read))
 
     val n = NatIdentifier(freshName("n"))
-    //val vecA = Identifier(freshName("vecA"), ExpType(ArrayType(n, float), read))
-    //val vecB = Identifier(freshName("vecB"), ExpType(ArrayType(n, float), read))
     val columnB = Identifier(freshName("columnB"), ExpType(ArrayType(n, float), read))
     val rowA = Identifier(freshName("rowA"), ExpType(ArrayType(n, float), read))
     val matrixA = Identifier(freshName("MatrixA"), ExpType(ArrayType(n, ArrayType(n,float)),read))
@@ -36,17 +33,11 @@ class matrixmultiplication extends test_util.Tests {
     val mul = Lambda[ExpType, ExpType](x, BinOp(Operators.Binary.MUL, Fst(float, float, x), Snd(float, float, x)))
     val add = Lambda[ExpType, FunType[ExpType, ExpType]](y, Lambda[ExpType, ExpType](z, BinOp(Operators.Binary.ADD, y, z)))
 
-    //val dot = DepLambda[NatKind](n)(Lambda[ExpType, FunType[ExpType, ExpType]](vecA, Lambda[ExpType, ExpType](vecB,
-      //ReduceSeq(n, float, float, add, Literal(FloatData(0.0f)),
-        //MapSeq(n, PairType(float, float), float, mul,
-          //Zip(n, float, float, vecA, vecB))))))
     val dotproduct = Lambda[ExpType, ExpType](rowA,
         ReduceSeq(n, float, float, add, Literal(FloatData(0.0f)),
           MapSeq(n, PairType(float, float), float, mul,
             Zip(n, float, float, rowA, columnB))))
 
-    //val vectorPair = Identifier(freshName("vectorPair"), ExpType(PairType(ArrayType(n,float), ArrayType(n,float)), read))
-    //val dotSeq = Lambda[ExpType, ExpType](vectorPair, dot)
     val matrixMult = DepLambda[NatKind](n)(Lambda[ExpType, FunType[ExpType, ExpType]](
       matrixA, Lambda[ExpType, ExpType](matrixB,
         MapSeq(n, ArrayType(n,float), ArrayType(n,float),
@@ -58,10 +49,6 @@ class matrixmultiplication extends test_util.Tests {
     ))
     println("matrixMult:\n " + ProgramGenerator.makeCode(matrixMult, "matrixMult").code)
 
-    /*
-    println(add(1,2))
-    add(8.0f,2.0f) shouldBe 10
-    */
   }
 
 
