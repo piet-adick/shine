476687a6206b343e5e651f06eeeb2414c77afa89
André <andre.hodapp@t-online.de>
2020-02-13 23:15:51 +0100

    maximum, minimum und kroenecker, aber kroenecker auf unschöne Weise gelöst

---
+++ src/test/scala/shine/examples/Kroenecker.scala
@@ -0,0 +1,45 @@
+package shine.examples
+
+import shine.C.ProgramGenerator
+import shine.DPIA.Phrases.{BinOp, Identifier, IfThenElse, Lambda, Operators}
+import shine.DPIA.Types.{ExpType, FunType, float, read}
+import shine.DPIA.freshName
+//import shine.DPIA.{NatIdentifier, freshName}
+
+class Kroenecker extends test_util.Tests{
+
+  test("maximum"){
+
+    //def kroenecker = fun(a,b) => a==b ? 1 : 0
+    val a = Identifier(freshName("a"), ExpType(float, read))
+    val b = Identifier(freshName("b"), ExpType(float, read))
+    val max = Lambda[ExpType, FunType[ExpType, ExpType]](a, Lambda[ExpType, ExpType](b, IfThenElse(BinOp(Operators.Binary.GT, a,b), a, b)))
+
+    println(ProgramGenerator.makeCode(max, "max").code)
+  }
+
+  test("minimum"){
+
+    //def kroenecker = fun(a,b) => a==b ? 1 : 0
+    val a = Identifier(freshName("a"), ExpType(float, read))
+    val b = Identifier(freshName("b"), ExpType(float, read))
+    val min = Lambda[ExpType, FunType[ExpType, ExpType]](a, Lambda[ExpType, ExpType](b, IfThenElse(BinOp(Operators.Binary.LT, a,b), a, b)))
+
+    println(ProgramGenerator.makeCode(min, "min").code)
+  }
+
+  test("kroenecker-umstaendlich"){
+
+    //def kroenecker = fun(a,b) => a==b ? 1 : 0
+    val a = Identifier(freshName("a"), ExpType(float, read))
+    val b = Identifier(freshName("b"), ExpType(float, read))
+    /*
+    bisher ist der Test noch umständlich, weil die Funktionen jeweils Phrases erwaten
+    und nicht einfach ein Float-Wert 1.0f als Eingabe, also als Ergebnis akzeptieren
+     */
+    val kroenecker = Lambda[ExpType, FunType[ExpType, ExpType]](a, Lambda[ExpType, ExpType](b, IfThenElse(BinOp(Operators.Binary.EQ, a,b), BinOp(Operators.Binary.DIV, a,a), BinOp(Operators.Binary.SUB, a,a))))
+
+    println(ProgramGenerator.makeCode(kroenecker, "kroenecker").code)
+  }
+
+}
