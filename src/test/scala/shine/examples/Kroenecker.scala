235e83ffc1206af1e54ec46d83c82998bcb25ec3
André <andre.hodapp@t-online.de>
2020-02-14 13:48:48 +0100

    kroenecker gelöst

--- src/test/scala/shine/examples/Kroenecker.scala
+++ src/test/scala/shine/examples/Kroenecker.scala
@@ -1,7 +1,8 @@
 package shine.examples
 
 import shine.C.ProgramGenerator
-import shine.DPIA.Phrases.{BinOp, Identifier, IfThenElse, Lambda, Operators}
+import shine.DPIA.Phrases.{BinOp, Identifier, IfThenElse, Lambda, Literal, Operators}
+import shine.DPIA.Semantics.OperationalSemantics.FloatData
 import shine.DPIA.Types.{ExpType, FunType, float, read}
 import shine.DPIA.freshName
 //import shine.DPIA.{NatIdentifier, freshName}
@@ -42,4 +43,17 @@ class Kroenecker extends test_util.Tests{
     println(ProgramGenerator.makeCode(kroenecker, "kroenecker").code)
   }
 
+  test("kroenecker"){
+
+    //def kroenecker = fun(a,b) => a==b ? 1 : 0
+    val a = Identifier(freshName("a"), ExpType(float, read))
+    val b = Identifier(freshName("b"), ExpType(float, read))
+    /*
+    bisher ist der Test noch umständlich, weil die Funktionen jeweils Phrases erwaten
+    und nicht einfach ein Float-Wert 1.0f als Eingabe, also als Ergebnis akzeptieren
+     */
+    val kroenecker = Lambda[ExpType, FunType[ExpType, ExpType]](a, Lambda[ExpType, ExpType](b, IfThenElse(BinOp(Operators.Binary.EQ, a,b), Literal(FloatData(1.0f)), Literal(FloatData(0.0f)))))
+
+    println(ProgramGenerator.makeCode(kroenecker, "kroenecker").code)
+  }
 }
