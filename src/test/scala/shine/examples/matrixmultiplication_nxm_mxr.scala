70c0896eba7cfcf5989959c43659ed4740cbd3c0
Lukas <lukassiefkehh@gmail.com>
2020-02-17 23:58:05 +0100

    Filterbeispiel, GEMM-Beispiel

--- src/test/scala/shine/examples/matrixmultiplication_nxm_mxr.scala
+++ src/test/scala/shine/examples/matrixmultiplication_nxm_mxr.scala
@@ -54,6 +54,46 @@ class matrixmultiplication_nxm_mxr extends test_util.Tests {
 
   }
 
+  test("gemm") {
+    val x = Identifier(freshName("x"), ExpType(PairType(float, float), read))
+    val y = Identifier(freshName("y"), ExpType(float, read))
+    val z = Identifier(freshName("z"), ExpType(float, read))
+
+    val n = NatIdentifier(freshName("n"))
+    val m = NatIdentifier(freshName("m"))
+    val k = NatIdentifier(freshName("k"))
+
+    val rowAC = Identifier(freshName("vecAC"), ExpType(PairType(ArrayType(m, float),ArrayType(k, float)), read))
+    val columnBFC = Identifier(freshName("columnBFC"), ExpType(PairType(ArrayType(m, float),float), read))
+    val matA = Identifier(freshName("matA"), ExpType(ArrayType(n, ArrayType(m, float)), read))
+    val matB = Identifier(freshName("matB"), ExpType(ArrayType(m, ArrayType(k, float)), read))
+    val matC = Identifier(freshName("matC"), ExpType(ArrayType(n, ArrayType(k, float)), read))
+
+    val mul = Lambda[ExpType, ExpType](x, BinOp(Operators.Binary.MUL, Fst(float, float, x), Snd(float, float, x)))
+    val add = Lambda[ExpType, FunType[ExpType, ExpType]](y, Lambda[ExpType, ExpType](z, BinOp(Operators.Binary.ADD, y, z)))
+
+    val gemm = DepLambda[NatKind](n)(DepLambda[NatKind](m)(DepLambda[NatKind](k)(
+      Lambda[ExpType, FunType[ExpType, FunType[ExpType, ExpType]]](matC,
+        Lambda[ExpType, FunType[ExpType, ExpType]](matA,
+          Lambda[ExpType, ExpType](matB,
+            MapSeq(n, PairType(ArrayType(m, float), ArrayType(k, float)), ArrayType(k, float),
+              Lambda[ExpType, ExpType] (rowAC,
+                MapSeq(k, PairType(ArrayType(m, float), float), float,
+                  Lambda[ExpType, ExpType] (columnBFC,
+                    BinOp(Operators.Binary.ADD,
+                      ReduceSeq(m, float, float, add, Literal(FloatData(0.0f)),
+                        MapSeq(m, PairType(float, float), float, mul,
+                          Zip(m, float, float,
+                            Fst(ArrayType(m,float), ArrayType(k, float), rowAC),
+                            Fst(ArrayType(m,float), float, columnBFC)))),
+                      Snd(ArrayType(m,float), float, columnBFC))),
+                  Zip(k, ArrayType(m, float), float,
+                    Transpose(m, k, float, matB), Snd(ArrayType(m,float), ArrayType(k, float), rowAC)))),
+              Zip(n, ArrayType(m, float), ArrayType(k, float), matA, matC))))))))
+
+    println(ProgramGenerator.makeCode(gemm, "gemm").code)
+  }
+
   test("matrixMult OpenCl") {
 
 
