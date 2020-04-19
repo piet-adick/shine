a8d7fe4f4632eb29bbf5030488af81f5a8ec6caf
visualJames <40604564+visualJames@users.noreply.github.com>
2020-04-19 16:55:56 +0200

    Create square.scala
    Hier sind die 3 Beispiele. Alle Beispiele sind problemlos auf meinem Computer durchgelaufen

---
+++ src/test/scala/shine/examples/square.scala
@@ -0,0 +1,83 @@
+package shine.examples
+
+import shine.C.ProgramGenerator
+import shine.DPIA.FunctionalPrimitives.{Drop, Fst, Join, MapSeq, ReduceSeq, Snd, Split}
+import shine.DPIA.Phrases.{BinOp, Identifier, Lambda, Literal, Operators}
+import shine.DPIA.Semantics.OperationalSemantics.FloatData
+import shine.DPIA.Types.{ArrayType, ExpType, FunType, PairType, float, read, write}
+import shine.DPIA.freshName
+
+  //the amount of a vector should be compared with another vector and
+  //the greater vector should be returned
+  class diagonale extends test_util.Tests {
+
+
+    //calculate the determinante of a 2x2 matrix als Paare
+    test("addiereNurErstenBeidenElementeMatrix") {
+      val x = Identifier(freshName("x"), ExpType(float, read))
+      val y = Identifier(freshName("y"), ExpType(float, read))
+      val add = Lambda[ExpType, FunType[ExpType, ExpType]](x, Lambda[ExpType, ExpType](y, BinOp(Operators.Binary.ADD, x, y)))
+
+
+      val matrix2x2 = Identifier(freshName("matrix2x2"), ExpType(ArrayType(2, ArrayType(2, float)), read))
+
+      val addiereNurErstenBeidenElementeMatrix = Lambda[ExpType, ExpType](matrix2x2,
+        BinOp(Operators.Binary.ADD,
+          ReduceSeq(2, float, float, add, Literal(FloatData(0.0f)), Join(1, 2, read, float, Drop(1, 1, read, ArrayType(2, float), matrix2x2)
+          )), Literal(FloatData(1.0f))))
+
+      println(ProgramGenerator.makeCode(addiereNurErstenBeidenElementeMatrix, "addiereNurErstenBeidenElementeMatrix").code)
+    }
+
+    //calculate the determinante of a 2x2 matrix als Paare
+    test("determinante2x2Pair") {
+      val matrix2x2PairPair = Identifier(freshName("matrix2x2"), ExpType(PairType(PairType(float, float), PairType(float, float)), read))
+
+      val determinante2x2Pair = Lambda[ExpType, ExpType](
+        matrix2x2PairPair,
+        BinOp(Operators.Binary.SUB,
+          //ad
+          BinOp(Operators.Binary.MUL,
+            //a
+            Fst(float, float, Fst(PairType(float, float), PairType(float, float),
+              matrix2x2PairPair
+            )),
+            //d
+            Snd(float, float, Snd(PairType(float, float), PairType(float, float),
+              matrix2x2PairPair
+            ))),
+          //bc
+          BinOp(Operators.Binary.MUL,
+            //a
+            Fst(float, float, Snd(PairType(float, float), PairType(float, float),
+              matrix2x2PairPair
+            )),
+            //d
+            Snd(float, float, Fst(PairType(float, float), PairType(float, float),
+              matrix2x2PairPair
+            )))
+
+        )
+
+      )
+      println(ProgramGenerator.makeCode(determinante2x2Pair, "determinante2x2Pair").code)
+    }
+
+
+    test("squareEveryElementInMatrix") {
+      val x = Identifier(freshName("x"), ExpType(float, read))
+      val square = Lambda[ExpType, ExpType](x, BinOp(Operators.Binary.MUL, x, x))
+      val matrix2x2 = Identifier(freshName("matrix2x2"), ExpType(ArrayType(2, ArrayType(2, float)), read))
+
+      val squareEveryElementInMatrix = Lambda[ExpType, ExpType](
+        matrix2x2,
+        Split(2, 2, read, float,
+          MapSeq(4, float, float, square, Join(2, 2, write, float,
+            matrix2x2
+          ))
+        )
+      )
+
+      println(ProgramGenerator.makeCode(squareEveryElementInMatrix, "squareEveryElementInMatrix").code)
+    }
+  }
