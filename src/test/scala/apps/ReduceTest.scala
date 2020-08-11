69b318c3c1d8de36f24a30977a34d3b6a0b5ff12
PietLinux <piet.adick@web.de>
2020-08-11 12:06:59 +0200

    reduce kernel: fixed padding, added idx(0) in warp reduce

--- src/test/scala/apps/ReduceTest.scala
+++ src/test/scala/apps/ReduceTest.scala
@@ -17,16 +17,21 @@ class ReduceTest extends shine.test_util.Tests {
 
       val id = fun(x => x)
 
-      // idx(0) für f32 als Rückgabe wäre schöner, da kein join nach reduceWarp erforderlich
+      // max block size in cuda ist 1024 threads (also 32 warps)
+
+      // idx(0) für f32 als Rückgabe ist schöner, da kein join nach reduceWarp erforderlich
       // für Implementierung mit shfl wäre dies sowieso erforderlich, da sonst 32.f32 als Rückgabe
       // funktioniert jedoch nicht
-      val reduceWarp = fun((32 `.` f32) ->: (1 `.` f32))(arr =>
+      // Fehlermeldung:
+      //  rise.core.types.InferenceException was thrown.
+      //  inference exception: expected a dependent function type, but got (idx[_n18] -> (_n18._dt19 -> _dt19))
+      val reduceWarp = fun((32 `.` f32) ->: f32)(arr =>
         arr |>
           split(32) |>
           mapLane('x')(fun(result =>
             result |>
               oclReduceSeq(AddressSpace.Private)(redop)(l(0f))
-          ))
+          )) |> idx(0)
       )
 
       fun(n `.` f32)(arr =>
@@ -38,86 +43,86 @@ class ReduceTest extends shine.test_util.Tests {
                   mapLane(fun(threadChunk =>
                     threadChunk |>
                       oclReduceSeq(AddressSpace.Private)(redop)(l(0f)))) |> // 32.f32
-                  toLocal |> // eigentlich toPrivate wegen shfl
-                  reduceWarp)) |> join |> // k/j.f32
+                  toLocal |> // eigentlich im private memory lassen wegen shfl
+                  reduceWarp)) |> // k/j.f32
               toLocal |>
-              padCst(0)(32-(k/j)/32)(l(0f)) |>
+              padCst(0)(32-(k/j))(l(0f)) |> // padde um 32-#Warps viele Elemente um auf 32 zu kommen
               split(32) |>
               mapWarp(fun(warpChunk =>
                 warpChunk |>
                   mapLane(id) |>
                   toLocal |> // eigentlich toPrivate wegen shfl
-                  reduceWarp)) |> join )))
+                  reduceWarp)))))
     }
 
     gen.cuKernel(testKernel, "reduceTest")
   }
 }
 
-//  Generierter Code:
-//  extern "C" __global__
-//  void reduceTest(float* __restrict__ output, const float* __restrict__ x0, __shared__ float* __restrict__ x213, __shared__ float* __restrict__ x264, __shared__ float* __restrict__ x243){
-//    /* Start of moved local vars */
-//    /* End of moved local vars */
-//    /* mapBlock */
-//    for (int block_id_374 = blockIdx.x;(block_id_374 < 4);block_id_374 = (block_id_374 + gridDim.x)) {
-//      /* mapWarp */
-//      for (int warp_id_377 = (threadIdx.x / 32);(warp_id_377 < 16);warp_id_377 = (warp_id_377 + (blockDim.x / 32))) {
-//        /* mapLane */
-//        /* iteration count is exactly 1, no loop emitted */
-//        int lane_id_381 = (threadIdx.x % 32);
-//        /* oclReduceSeq */
-//        {
-//          float x276;
-//          x276 = 0.0f;
-//          for (int i_383 = 0;(i_383 < 4);i_383 = (1 + i_383)) {
-//          x276 = (x276 + x0[(((i_383 + (4 * lane_id_381)) + (128 * warp_id_377)) + (2048 * block_id_374))]);
-//        }
+//  Generierter Code: (ohne idx(0))
+//extern "C" __global__
+//void reduceTest(float* __restrict__ output, const float* __restrict__ x0, __shared__ float* __restrict__ x215, __shared__ float* __restrict__ x266, __shared__ float* __restrict__ x245){
+//  /* Start of moved local vars */
+//  /* End of moved local vars */
+//  /* mapBlock */
+//  for (int block_id_376 = blockIdx.x;(block_id_376 < 4);block_id_376 = (block_id_376 + gridDim.x)) {
+//  /* mapWarp */
+//  for (int warp_id_379 = (threadIdx.x / 32);(warp_id_379 < 16);warp_id_379 = (warp_id_379 + (blockDim.x / 32))) {
+//  /* mapLane */
+//  /* iteration count is exactly 1, no loop emitted */
+//  int lane_id_383 = (threadIdx.x % 32);
+//  /* oclReduceSeq */
+//{
+//  float x278;
+//  x278 = 0.0f;
+//  for (int i_385 = 0;(i_385 < 4);i_385 = (1 + i_385)) {
+//  x278 = (x278 + x0[(((i_385 + (4 * lane_id_383)) + (128 * warp_id_379)) + (2048 * block_id_376))]);
+//}
 //
-//          x264[lane_id_381] = x276;
-//        }
+//  x266[lane_id_383] = x278;
+//}
 //
-//        /* mapLane */
-//        for (int lane_id_382 = (threadIdx.x % 32);(lane_id_382 < 1);lane_id_382 = (32 + lane_id_382)) {
-//          /* oclReduceSeq */
-//          {
-//            float x257;
-//            x257 = 0.0f;
-//            for (int i_384 = 0;(i_384 < 32);i_384 = (1 + i_384)) {
-//            x257 = (x257 + x264[(i_384 + (32 * lane_id_382))]);
-//          }
+//  /* mapLane */
+//  for (int lane_id_384 = (threadIdx.x % 32);(lane_id_384 < 1);lane_id_384 = (32 + lane_id_384)) {
+//  /* oclReduceSeq */
+//{
+//  float x259;
+//  x259 = 0.0f;
+//  for (int i_386 = 0;(i_386 < 32);i_386 = (1 + i_386)) {
+//  x259 = (x259 + x266[(i_386 + (32 * lane_id_384))]);
+//}
 //
-//            x243[(lane_id_382 + warp_id_377)] = x257;
-//          }
+//  x245[(lane_id_384 + warp_id_379)] = x259;
+//}
 //
-//        }
+//}
 //
-//      }
+//}
 //
-//      /* mapWarp */
-//      for (int warp_id_380 = (threadIdx.x / 32);(warp_id_380 < (6144 / 4096));warp_id_380 = (warp_id_380 + (blockDim.x / 32))) {
-//        /* mapLane */
-//        /* iteration count is exactly 1, no loop emitted */
-//        int lane_id_385 = (threadIdx.x % 32);
-//        x213[lane_id_385] = ((((lane_id_385 + (32 * warp_id_380)) < 16)) ? (x243[(lane_id_385 + (32 * warp_id_380))]) : (0.0f));
-//        /* mapLane */
-//        for (int lane_id_386 = (threadIdx.x % 32);(lane_id_386 < 1);lane_id_386 = (32 + lane_id_386)) {
-//          /* oclReduceSeq */
-//          {
-//            float x206;
-//            x206 = 0.0f;
-//            for (int i_387 = 0;(i_387 < 32);i_387 = (1 + i_387)) {
-//            x206 = (x206 + x213[(i_387 + (32 * lane_id_386))]);
-//          }
+//  /* mapWarp */
+//  for (int warp_id_382 = (threadIdx.x / 32);(warp_id_382 < 1);warp_id_382 = (warp_id_382 + (blockDim.x / 32))) {
+//  /* mapLane */
+//  /* iteration count is exactly 1, no loop emitted */
+//  int lane_id_387 = (threadIdx.x % 32);
+//  x215[lane_id_387] = ((((lane_id_387 + (32 * warp_id_382)) < 16)) ? (x245[(lane_id_387 + (32 * warp_id_382))]) : (0.0f));
+//  /* mapLane */
+//  for (int lane_id_388 = (threadIdx.x % 32);(lane_id_388 < 1);lane_id_388 = (32 + lane_id_388)) {
+//  /* oclReduceSeq */
+//{
+//  float x208;
+//  x208 = 0.0f;
+//  for (int i_389 = 0;(i_389 < 32);i_389 = (1 + i_389)) {
+//  x208 = (x208 + x215[(i_389 + (32 * lane_id_388))]);
+//}
 //
-//            output[((lane_id_386 + warp_id_380) + ((6144 * block_id_374) / 4096))] = x206;
-//          }
+//  output[((block_id_376 + lane_id_388) + warp_id_382)] = x208;
+//}
 //
-//        }
+//}
 //
-//      }
+//}
 //
-//    }
+//}
 //
-//    __syncthreads();
-//  }
+//  __syncthreads();
+//}
